afad714b9348a6d8fa630ad30217f3d2324d2766
Kurt Kuhn <kurtajkuhn@gmail.com>
2024-07-09 14:28:25 -0700

    Updated javascript file for the proper syntax

--- resume/js/scripts.js
+++ resume/js/scripts.js
@@ -18,12 +18,13 @@ async function getSubDomain(){
         const domain = document.querySelector(".vistorcount")
         const subdomain = window.location.hostname.split('.').slice(0, -2).join('.');
         const apigw_url = "https://bgow867901.execute-api.us-east-1.amazonaws.com/default/VisitorCounter"
-        uri = apigw_url  + "?subdomain=" + subdomain
+        uri = apigw_url  + "?subdomain=" + subdomain;
         
         let response = await fetch(uri, {method: 'GET',
         });
         let data = await response.json();
 
+        console.log(data);
         console.log(data);
         count = data['resume_count'];
         console.log('count: ' + count);
